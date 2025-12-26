7cd94779b3d0790e10bdc94cf3fc0638dd0e56f2
Ayush Singh <ayushsingh18777@gmail.com>
2025-12-26 22:40:41 +0530

    Factorial of a number

---
+++ Assignment_6/factorial.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int i, fact = 1,n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    for(i=1; i<=n;i++){
+        fact=fact*i;
+    }
+    printf("The factorial of %d is %d", n , fact);
+}
