fb325bae657e9a8ba598c8e1bd0f3d8d36d5f6ee
Ayush Singh <ayushsingh18777@gmail.com>
2025-12-27 09:24:20 +0530

    leap year program

---
+++ Day_5/leap_year.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int year;
+    printf("Enter the year : ");
+    scanf("%d", &year);
+    if(year % 400 ==0 || (year % 4==0 && year % 100 != 0)){
+        printf("This year is a leap year");
+    }
+    else{
+        printf("This year is not a leap year");
+    }
+}
