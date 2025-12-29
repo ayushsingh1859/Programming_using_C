bbe88f90272c70f10178b0e36e92bc58bc354763
Ayush Singh <ayushsingh18777@gmail.com>
2025-12-29 22:16:46 +0530

    sum of n digit numbers  program

---
+++ Day_4/sum_of_digits.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n , sum = 0;
+    printf("Enter the 5 digit number : ");
+    scanf("%d", &n);
+    while(n>0){
+        sum+=n%10;
+        n/=10;
+    }
+    printf("the sum is %d",sum);
+    return 0;
+}
